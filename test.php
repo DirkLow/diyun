3b7d0dd322009a352dca63d5f6f5171176661efe
Dirk <diyun@b5m.com>
2016-05-17 16:28:50 +0800

    second commit

---
+++ test.php
@@ -0,0 +1,30 @@
+<!-- <?php 
+
+	testXdebug();
+	function testXdebug() {
+		requireFile();
+	}
+	function requireFile() {
+		require_once('abc.php');
+	}
+	// phpinfo();
+
+
+ ?> -->
+
+ <?php
+/**
+* Simple function to replicate PHP 5 behaviour
+*/
+function microtime_float()
+{
+      list($usec, $sec) = explode(" ", microtime());
+return ((float)$usec + (float)$sec);
+}
+$time_start = microtime_float();
+// Sleep for a while
+usleep(100);
+$time_end = microtime_float();
+$time = $time_end - $time_start;
+echo "Did nothing in $time seconds\n";
+?>
